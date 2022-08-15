3507a460237f698bedff081f92a2672902b5f427
Maxime Bailleux <max.bailleux.13@gmail.com>
2022-08-16 01:25:18 +0200

    Restore local storage tests.

--- src/boot/local-storage.ts
+++ src/boot/local-storage.ts
@@ -1,52 +1,8 @@
-import { useSettingsStore } from 'stores/settings';
-import { useTalismanStore } from 'stores/talismans';
-import * as localforage from 'localforage';
-import { Talisman } from 'src/models/talisman';
-import _each from 'lodash/each';
 import { boot } from 'quasar/wrappers';
-import { i18n } from 'boot/i18n';
+import initLocalStorageForSettingsStore from '../utils/local-storage-settings';
+import initLocalStorageForTalismanStore from '../utils/local-storage-talismans';
 
 export default boot(async () => {
-  const talismanStore = useTalismanStore();
-  const settingsStore = useSettingsStore();
-
-  /**
-   * Convert parsed string to Talismans
-   * @param value
-   */
-  function toTalismans(value: string | null) {
-    talismanStore.$reset();
-    _each(JSON.parse(value || '[]'), (data) => {
-      talismanStore.talismans.push(new Talisman({ ...data }));
-    });
-  }
-
-  // Load cache for Talisman's list
-  localforage.getItem<string>('mhrs-talismans')
-    .then((value) => toTalismans(value));
-
-  // Load cache for settings
-  localforage.getItem<string>('mhrs-settings')
-    .then((value) => JSON.parse(value || '{}'))
-    .then((value) => {
-      // Initialize locale settings
-      if (value.locale) {
-        settingsStore.locale = value.locale;
-        i18n.global.locale.value = settingsStore.locale;
-      }
-    });
-
-  // Talisman store subscriber to save each modification in cache
-  talismanStore.$onAction(({ after }) => {
-    after(() => {
-      localforage.setItem('mhrs-talismans', JSON.stringify(talismanStore.talismans));
-    });
-  });
-
-  // Settings store subscriber to save each modification in cache
-  settingsStore.$onAction(({ after }) => {
-    after(() => {
-      localforage.setItem('mhrs-settings', JSON.stringify(settingsStore.$state));
-    });
-  });
+  initLocalStorageForSettingsStore();
+  initLocalStorageForTalismanStore();
 });
---
+++ test/utils/local-storage-talismans.spec.ts
@@ -0,0 +1,106 @@
+import {
+  afterEach,
+  describe, expect, it, jest,
+} from '@jest/globals';
+import { Talisman } from 'src/models/talisman';
+import { createPinia, setActivePinia } from 'pinia';
+import { useSkillStore } from 'stores/skills';
+import { useSlotsStore } from 'stores/slots';
+import localforage from 'localforage';
+import { config, flushPromises } from '@vue/test-utils';
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { useTalismanStore } from 'stores/talismans';
+import { i18n } from 'boot/i18n';
+import initLocalStorageForTalismanStore from 'src/utils/local-storage-talismans';
+
+installQuasarPlugin();
+
+jest.mock('boot/i18n');
+jest.mock('localforage');
+
+describe('utils/local-storage-talismans', () => {
+  config.global.mocks.$t = i18n.global.t;
+  config.global.plugins = [...config.global.plugins, i18n];
+
+  setActivePinia(createPinia());
+  const { getSkillById } = useSkillStore();
+  const { getSlotsById } = useSlotsStore();
+  const talismanStore = useTalismanStore();
+
+  const allTalismans = [
+    new Talisman({
+      skill1: getSkillById('weakness-exploit'),
+      skill1Level: 2,
+      slots: getSlotsById('2-1-0'),
+    }),
+    new Talisman({
+      skill1: getSkillById('attack-boost'),
+      skill1Level: 2,
+      skill2: getSkillById('slugger'),
+      skill2Level: 1,
+      slots: getSlotsById('1-1-0'),
+    }),
+    new Talisman({
+      skill1: getSkillById('speed-sharpening'),
+      skill1Level: 1,
+      skill2: getSkillById('weakness-exploit'),
+      skill2Level: 1,
+      slots: getSlotsById('0-0-0'),
+      favorite: true,
+    }),
+    new Talisman({
+      skill1: getSkillById('master-mounter'),
+      skill1Level: 1,
+      skill2: getSkillById('slugger'),
+      skill2Level: 1,
+      slots: getSlotsById('1-1-0'),
+      forMelting: true,
+    }),
+  ];
+
+  afterEach(() => {
+    talismanStore.$reset();
+    jest.clearAllMocks();
+  });
+
+  describe('when no data is stored in cache', () => {
+    it('load empty cache to store', async () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line no-underscore-dangle
+      localforage.__setMockData__('');
+      initLocalStorageForTalismanStore();
+      await flushPromises();
+      expect(talismanStore.talismans).toStrictEqual([]);
+    });
+  });
+
+  describe('when data is stored in cache', () => {
+    it('load cached talismans to store', async () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line no-underscore-dangle
+      localforage.__setMockData__(JSON.stringify(allTalismans));
+      initLocalStorageForTalismanStore();
+      await flushPromises();
+      expect(talismanStore.talismans).toStrictEqual(allTalismans);
+    });
+  });
+
+  describe('when store update his data with an action', () => {
+    it('data is stored in cache', async () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line no-underscore-dangle
+      localforage.__setMockData__('');
+      initLocalStorageForTalismanStore();
+      const newTalisman = new Talisman({
+        skill1: getSkillById('weakness-exploit'),
+        skill1Level: 2,
+        slots: getSlotsById('2-1-0'),
+      });
+      talismanStore.addTalisman(newTalisman);
+      expect(localforage.setItem).toHaveBeenCalledWith('mhrs-talismans', JSON.stringify([newTalisman]));
+    });
+  });
+});
