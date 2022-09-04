ee1e45a35b869941872790eedfd332d0c47ee2f4
Maxime Bailleux <max.bailleux.13@gmail.com>
2022-09-04 15:19:53 +0200

    Fixed Talisman Import tab style.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -22,6 +22,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 - Fixed Talisman card style.
+- Fixed Talisman Import tab style.
 
 ## [0.3.0] - 2022-08-24
 
--- src/components/ManagerImport.vue
+++ src/components/ManagerImport.vue
@@ -14,7 +14,6 @@ const talismanList = ref('');
 const { errorsFromImport, talismansToImport } = useTalismanImport(talismanList);
 
 const submitDisabled = computed(() => talismansToImport.value.length <= 0);
-const splitterModel = ref(50);
 
 function submitImport() {
   if (talismansToImport.value) {
@@ -36,71 +35,65 @@ function submitImport() {
     <h2 class="text-h6">
       {{ $t('manager.import.title') }}
     </h2>
-    <q-splitter
-      v-model="splitterModel"
-    >
-      <template #before>
-        <div class="q-pa-md">
-          <q-form>
-            <q-input
-              v-model="talismanList"
-              outlined
-              type="textarea"
+    <div class="row">
+      <div class="q-pa-md col-sm-12 col-md-6">
+        <q-form>
+          <q-input
+            v-model="talismanList"
+            outlined
+            type="textarea"
+          />
+          <div class="q-ma-md">
+            <q-btn
+              :label="$t('manager.import.submit')"
+              type="submit"
+              :disable="submitDisabled"
+              @click="submitImport"
             />
-            <div class="q-ma-md">
-              <q-btn
-                :label="$t('manager.import.submit')"
-                type="submit"
-                :disable="submitDisabled"
-                @click="submitImport"
-              />
-              <span class="q-pl-md">
-                {{ $t('manager.import.talismans_to_import', talismansToImport.length, { count: talismansToImport.length }) }}
-              </span>
-            </div>
-          </q-form>
-        </div>
-      </template>
-      <template #after>
-        <div class="q-pa-md">
-          <manager-import-list-error
-            message="manager.import.errors.skill1.not_found"
-            :talismans-in-errors="errorsFromImport.skill1NotFound"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill1.is_empty"
-            :talismans-in-errors="errorsFromImport.skill1IsEmpty"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill1Level.is_empty"
-            :talismans-in-errors="errorsFromImport.skill1LevelIsEmpty"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill1Level.exceeds_maximum"
-            :talismans-in-errors="errorsFromImport.skill1LevelExceedsMaximum"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill2.not_found"
-            :talismans-in-errors="errorsFromImport.skill2NotFound"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill2.is_empty"
-            :talismans-in-errors="errorsFromImport.skill2IsEmpty"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill2Level.is_empty"
-            :talismans-in-errors="errorsFromImport.skill2LevelIsEmpty"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.skill2Level.exceeds_maximum"
-            :talismans-in-errors="errorsFromImport.skill2LevelExceedsMaximum"
-          />
-          <manager-import-list-error
-            message="manager.import.errors.slots.not_found"
-            :talismans-in-errors="errorsFromImport.slotsNotFound"
-          />
-        </div>
-      </template>
-    </q-splitter>
+            <span class="q-pl-md">
+              {{ $t('manager.import.talismans_to_import', talismansToImport.length, { count: talismansToImport.length }) }}
+            </span>
+          </div>
+        </q-form>
+      </div>
+      <div class="q-pa-md col-sm-12 col-md-6">
+        <manager-import-list-error
+          message="manager.import.errors.skill1.not_found"
+          :talismans-in-errors="errorsFromImport.skill1NotFound"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill1.is_empty"
+          :talismans-in-errors="errorsFromImport.skill1IsEmpty"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill1Level.is_empty"
+          :talismans-in-errors="errorsFromImport.skill1LevelIsEmpty"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill1Level.exceeds_maximum"
+          :talismans-in-errors="errorsFromImport.skill1LevelExceedsMaximum"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill2.not_found"
+          :talismans-in-errors="errorsFromImport.skill2NotFound"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill2.is_empty"
+          :talismans-in-errors="errorsFromImport.skill2IsEmpty"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill2Level.is_empty"
+          :talismans-in-errors="errorsFromImport.skill2LevelIsEmpty"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.skill2Level.exceeds_maximum"
+          :talismans-in-errors="errorsFromImport.skill2LevelExceedsMaximum"
+        />
+        <manager-import-list-error
+          message="manager.import.errors.slots.not_found"
+          :talismans-in-errors="errorsFromImport.slotsNotFound"
+        />
+      </div>
+    </div>
   </div>
 </template>
